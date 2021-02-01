856bee0889dc4dbc09b18b8f7d28d36406824c64
bishkou <chedyhammami@yahoo.com>
2021-02-01 18:04:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import hashlib
+import getpass
+
+
+def checkPass(pwd):
+    pwd = pwd.strip()
+    hashedPass = hashlib.sha1(pwd.encode('utf-8')).hexdigest()
+    firstFive = hashedPass[:5]
+    rest = hashedPass[5:].upper()
+    url = 'https://api.pwnedpasswords.com/range/' + firstFive
+    res = requests.get(url)
+    if not res.ok:
+        raise RuntimeError('Error fetching "{}": {}'.format(
+            url, res.status_code))
+
+    for line in res.text.splitlines():
+        # hashes[0] : The rest of the hash
+        # hashes[1] : The number of occurrence
+        hashes = line.split(':')
+        if hashes[0] == rest:
+            return hashes[1]
+
+    return 0
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pwd = getpass.getpass()
+
+    try:
+        count = checkPass(pwd)
+    except:
+        print('Something broke, please try again')
+
+    if count:
+        print(f'Your Password was found with {count} occurrences')
+    else:
+        print('Your Password wasn\'t found.'
+              ' That doesn\'t necessarily mean it\'s a good password,'
+              ' merely that it\'s not indexed on this site')
+
+    # print(res.text)
